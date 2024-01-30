ab77cdf2fec3f4ed8aa91ad5284a96e66a26f406
gueripep <paul.gueripel@etu.univ-grenoble-alpes.fr>
2024-01-30 15:12:19 +0100

    correctifs offrecovoiturage

--- src/main/java/fr/uga/miage/m1/mapper/OffreCovoiturageMapper.java
+++ src/main/java/fr/uga/miage/m1/mapper/OffreCovoiturageMapper.java
@@ -5,9 +5,8 @@ import org.mapstruct.Mapper;
 import fr.uga.miage.m1.dto.OffreCovoiturageDto;
 import fr.uga.miage.m1.entities.OffreCovoiturage;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {FestivalMapper.class, AdherentMapper.class})
 public interface OffreCovoiturageMapper {
     OffreCovoiturageDto toDto(OffreCovoiturage festival);
-
     OffreCovoiturage toEntity(OffreCovoiturageDto dto);
 }
